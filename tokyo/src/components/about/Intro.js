d817a093bc7c42210ef002dc3aa323c78f150c54
Sofus Wenøe <44342080+Sofus-Wen@users.noreply.github.com>
2023-10-12 17:54:09 +0200

    Update Intro.js

--- tokyo/src/components/about/Intro.js
+++ tokyo/src/components/about/Intro.js
@@ -6,7 +6,7 @@ const Intro = () => {
         <img className="min-w-full" src="assets/img/slider/1.jpg" alt="image" />
       </div>
       <div className="about_title w-full h-auto clear-both float-left border-solid border-[#DFDFDF] border-b pb-[20px] mb-[30px]">
-        <h3 className="text-[22px] font-bold">Adriano Smith</h3>
+        <h3 className="text-[22px] font-bold">Sofus Wenøe</h3>
         <span>Web Developer</span>
       </div>
       <div className="about_text w-full h-auto clear-both float-left border-solid border-[#DFDFDF] border-b pb-[31px] mb-[30px]">
