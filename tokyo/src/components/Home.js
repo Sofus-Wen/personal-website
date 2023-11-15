a4ac6ddc63c4910ef19b3778eccbd62ae4b1fdbe
Sofus Wenøe <44342080+Sofus-Wen@users.noreply.github.com>
2023-11-15 18:44:32 +0100

    Update Home.js

--- tokyo/src/components/Home.js
+++ tokyo/src/components/Home.js
@@ -17,7 +17,7 @@ const socialIcon = [
   },
   {
     id: 4,
-    iconName: "/tokyo/public/assets/img/svg/social/substack-icon.svg",
+    iconName: "icon-mail",
     link: "https://sofus.substack.com",
   },
 ];
