8552613e87f7d89ba9aca63f1f162c7c383c0e31
Sofus Wenøe <44342080+Sofus-Wen@users.noreply.github.com>
2024-02-22 12:55:25 -0500

    Update Contact.js

--- tokyo/src/components/Contact.js
+++ tokyo/src/components/Contact.js
@@ -13,21 +13,32 @@ const Contact = () => {
             </div>
           </div>
           <Map />
-          {/* Cal inline embed code begins */}
-          <div style={{width: '100%', height: '100%', overflow: 'scroll'}} id="my-cal-inline"></div>
-          <script type="text/javascript">
-            (function (C, A, L) { let p = function (a, ar) { a.q.push(ar); }; let d = C.document; C.Cal = C.Cal || function () { let cal = C.Cal; let ar = arguments; if (!cal.loaded) { cal.ns = {}; cal.q = cal.q || []; d.head.appendChild(d.createElement("script")).src = A; cal.loaded = true; } if (ar[0] === L) { const api = function () { p(api, arguments); }; const namespace = ar[1]; api.q = api.q || []; typeof namespace === "string" ? (cal.ns[namespace] = api) && p(api, ar) : p(cal, ar); return; } p(cal, ar); }; })(window, "https://app.cal.com/embed/embed.js", "init");
-            Cal("init",  {origin:"https://cal.com"});
+          /* First make sure that you have installed the package */
 
-            Cal("inline", {
-              elementOrSelector:"#my-cal-inline",
-              calLink: "sofuswenoee/sofus-1-on-1",
-              layout: "month_view"
-            });
+          /* If you are using yarn */
+          // yarn add @calcom/embed-react
 
-            Cal("ui", {"styles":{"branding":{"brandColor":"#000000"}},"hideEventTypeDetails":false,"layout":"month_view"});
-          </script>
-          {/* Cal inline embed code ends */}
+          /* If you are using npm */
+          // npm install @calcom/embed-react
+
+          import Cal, { getCalApi } from "@calcom/embed-react";
+          import { useEffect } from "react";
+          export default function MyApp() {
+        	useEffect(()=>{
+        	  (async function () {
+        		const cal = await getCalApi();
+        		cal("ui", {"styles":{"branding":{"brandColor":"#000000"}},"hideEventTypeDetails":false,"layout":"month_view"});
+        	  })();
+        	}, [])
+        	return <Cal
+        	  calLink="sofuswenoee/sofus-1-on-1"
+        	  style={{width:"100%",height:"100%",overflow:"scroll"}}
+        	  config={{layout: 'month_view'}}
+
+
+        	/>;
+          };
+
           <ContactForm />
         </div>
       </div>
