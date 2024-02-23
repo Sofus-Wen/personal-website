d69621e8ee91c3c04f893e12b2698de323808c8a
Sofus Wenøe <44342080+Sofus-Wen@users.noreply.github.com>
2024-02-22 19:46:36 -0500

    Update Intro.js

--- tokyo/src/components/about/Intro.js
+++ tokyo/src/components/about/Intro.js
@@ -11,19 +11,7 @@ const Intro = () => {
       </div>
       <div className="about_text w-full h-auto clear-both float-left border-solid border-[#DFDFDF] border-b pb-[31px] mb-[30px]">
         <p className="mb-[11px]">
-          Welcome to my little corner of the internet! I'm 17, and like you, I'm on this wild journey of turning curiosity into amazing projects.
-        </p><br />
-        <p>
-          You ready? Let's jump into it!
-        </p><br />
-        <p>
-          At this point in my journey, I'm fully focused on seamlessly integrating AI with the realm of education. Picture transforming the learning experience into something as captivating and fulfilling as discovering breathtaking landscapes in your dreamlike destination – that's my mission right now. And you know what? I've ventured into coding, sparked my own business ventures, and now I'm confidently following my curiosity as I thoroughly explore the incredible possibilities of AI.
-        </p><br />
-        <p>
-          In the midst of this exciting journey, I'm actively immersing myself in the timeless wisdom of philosophy. This is about synergizing the finest from two realms – cutting-edge technology and age-old wisdom – to craft something genuinely valuable and timeless.
-        </p><br />
-        <p>
-          One of my specialties is taking an idea from scratch and creating a full-fledged platform. I go beyond to produce sites with a unique, outstanding, contemporary look-and-feel. With extensive knowledge of web mechanics, I’m able to optimize complex integrations to require little-to-no maintenance while running on their own for years.
+        Welcome back to my little corner of the internet! For those who don’t know me, my name is Sofus & I am a 17-year-old high school student interested in AI’s transformative possibilities on our future education system.
         </p>
       </div>
       {/*<div className="tokyo_tm_short_info w-full h-auto clear-both float-left flex border-solid border-[#DFDFDF] border-b pb-[30px] mb-[40px]">
