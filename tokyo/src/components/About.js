11d85dd90abdd1dc28c9d4b63cd96d48ec3806a2
Sofus Wenøe <44342080+Sofus-Wen@users.noreply.github.com>
2024-02-23 18:54:45 -0500

    Update About.js

--- tokyo/src/components/About.js
+++ tokyo/src/components/About.js
@@ -10,7 +10,7 @@ const About = () => {
   return (
     <SectionContainer name={"about"}>
       <div className="container">
-        <div className="tokyo_tm_about w-full h-auto clear-both float-left px-100-top px-0">
+        <div className="tokyo_tm_about w-full h-auto clear-both float-left px-0 px-100-top">
           <div className="tokyo_tm_title w-full h-auto clear-both float-left mb-[62px]">
             <div className="title_flex w-full h-auto clear-both flex justify-between items-end">
               <SectionTitle pageName={"About"} title={"About Me"} />
