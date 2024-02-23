2cdc26360dfc32e10813604fa0605c9fa9dc6743
Sofus Wenøe <44342080+Sofus-Wen@users.noreply.github.com>
2024-02-22 20:51:39 -0500

    Update Intro.js

--- tokyo/src/components/about/Intro.js
+++ tokyo/src/components/about/Intro.js
@@ -12,8 +12,8 @@ const Intro = () => {
       <div className="about_text w-full h-auto clear-both float-left border-solid border-[#DFDFDF] border-b pb-[31px] mb-[30px]">
         <p className="mb-[11px]">
           Welcome back to my little corner of the internet! For those who don’t know me, my name is Sofus, and I am a 17-year-old high school student deeply fascinated by the transformative possibilities of AI on our future education system. I want to redefine the landscape of learning, making it more personalized, effective, and, above all, meaningful.
-          {"\n"}
-          {"\n"}
+          <br />
+          <br />
           By integrating AI into education, I envision a future where learning is not a one-size-fits-all model but a tailored experience that meets individual needs, ignites passions, and unlocks the full potential of every learner.
         </p>
       </div>
