0443280ec10fc62d24a186b4ad486ebf3cfdb384
Sofus Wenøe <44342080+Sofus-Wen@users.noreply.github.com>
2024-02-24 00:49:42 -0500

    Update Testimonials.js

--- tokyo/src/components/about/Testimonials.js
+++ tokyo/src/components/about/Testimonials.js
@@ -30,19 +30,21 @@ const Testimonials = () => {
     <div className="tokyo_tm_testimonials w-full h-auto clear-both float-left py-[100px] px-0">
       <div className="container">
         <div id="testimonials-swiper-container" className="list w-full h-auto clear-both float-left overflow-hidden">
-          <Swiper
-            {...sliderProps.testimonial}
-            spaceBetween={50}
-            slidesPerView={3}
-            loop={true}
-            centeredSlides={true}
-            className="m-0 list-none cursor-e-resize">
-            {testimonialData.map((item) => (
-              <SwiperSlide key={item.id} className="flex justify-center">
-                <img src={item.authorImage} alt={`Company ${item.id}`} className="company-image" />
-              </SwiperSlide>
-            ))}
-          </Swiper>
+        <Swiper
+        {...sliderProps.testimonial}
+        spaceBetween={50}
+        slidesPerView={3} // Display 3 logos at a time
+        loop={true}
+        centeredSlides={true}
+        className="m-0 list-none cursor-e-resize"
+        // Additional Swiper settings as needed
+        >
+        {testimonialData.map((item) => (
+          <SwiperSlide key={item.id} className="flex justify-center items-center">
+            <img src={item.authorImage} alt={`Company ${item.id}`} className="company-image" />
+          </SwiperSlide>
+        ))}
+        </Swiper>
         </div>
       </div>
     </div>
