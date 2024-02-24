b929f9e73ed4998ebcd824141b85b8e1298caf1d
Sofus Wenøe <44342080+Sofus-Wen@users.noreply.github.com>
2024-02-24 00:17:32 -0500

    Summary commit

--- tokyo/src/components/about/KnowledgeInterests.js
+++ tokyo/src/components/about/KnowledgeInterests.js
@@ -1,62 +1,68 @@
-import React from 'react';
-import Slider from 'react-slick';
-
-const companies = [
-  { id: 1, title: "Company 1", image: "/assets/img/companies/company1.png" },
-  { id: 2, title: "Company 2", image: "/assets/img/companies/company2.png" },
-  { id: 3, title: "Company 3", image: "/assets/img/companies/company3.png" },
-  { id: 4, title: "Company 4", image: "/assets/img/companies/company4.png" },
-  { id: 5, title: "Company 5", image: "/assets/img/companies/company5.png" },
+const knowledge = [
+  { id: 1, text: "Bootstrap, Angular" },
+  { id: 2, text: "React, Vue, Laravel" },
+  { id: 3, text: "Stylus, Sass, Less" },
+  { id: 4, text: "Gulp, Webpack, Grunt" },
+  { id: 5, text: "Tweenmax, GSAP" },
+];
+const interests = [
+  { id: 1, text: "Make UI/UX Design" },
+  { id: 2, text: "Create Mobile App" },
+  { id: 3, text: "Site Optimization" },
+  { id: 4, text: "Custom Website" },
+  { id: 5, text: "Learn Ecommerce" },
 ];
 
-const CarouselComponent = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  };
-
+const KnowledgeInterests = () => {
   return (
-    <div className="carousel-wrapper">
-      <Slider {...settings}>
-        {companies.map((company) => (
-          <div key={company.id} className="company-slide">
-            <img src={company.image} alt={company.title} />
-            <h3>{company.title}</h3>
+    <div className="tokyo_tm_skillbox w-full h-auto clear-both float-left pt-[90px] pr-[0px] pb-[90px] pl-[0px]">
+      <div className="container">
+        <div className="in w-full h-auto clear-both float-left flex">
+          <div className="left w-1/2 pr-[50px]">
+            <div className="tokyo_section_title w-full h-auto clear-both float-left mb-[40px]">
+              <h3 className="text-[20px] font-bold">Knowledge</h3>
+            </div>
+            <div className="tokyo_tm_skill_list w-full h-auto clear-both float-left">
+              <ul className="m-0 list-none">
+                {knowledge.map((item) => (
+                  <li className="m-0 pl-[25px] relative" key={item.id}>
+                    <span>
+                      <img
+                        className="svg text-black w-[10px] h-[10px] absolute left-0 top-1/2 translate-y-[-50%]"
+                        src="assets/img/svg/rightarrow.svg"
+                        alt="image"
+                      />
+                      {item.text}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-        ))}
-      </Slider>
+          <div className="right w-1/2 pl-[50px]">
+            <div className="tokyo_section_title w-full h-auto clear-both float-left mb-[40px]">
+              <h3 className="text-[20px] font-bold">Interests</h3>
+            </div>
+            <div className="tokyo_tm_skill_list w-full h-auto clear-both float-left">
+              <ul className="m-0 list-none">
+                {interests.map((item) => (
+                  <li className="m-0 pl-[25px] relative" key={item.id}>
+                    <span>
+                      <img
+                        className="svg text-black w-[10px] h-[10px] absolute left-0 top-1/2 translate-y-[-50%]"
+                        src="assets/img/svg/rightarrow.svg"
+                        alt="image"
+                      />
+                      {item.text}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
-
-export default CarouselComponent;
+export default KnowledgeInterests;
