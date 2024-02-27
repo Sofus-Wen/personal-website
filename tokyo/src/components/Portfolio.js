27c1df2c01655e174646d581193bb08a6ad6fdd4
Sofus Wenøe <44342080+Sofus-Wen@users.noreply.github.com>
2024-02-27 10:32:39 -0500

    Update Portfolio.js

--- tokyo/src/components/Portfolio.js
+++ tokyo/src/components/Portfolio.js
@@ -114,7 +114,7 @@ const Portfolio = () => {
             <div className="title_flex w-full h-auto clear-both flex justify-between items-end">
               <SectionTitle
                 pageName={"Portfolio"}
-                title={"Creative Portfolio"}
+                title={"My Projects"}
               />
               <div className="portfolio_filter">
                 <ul className="list-none">
