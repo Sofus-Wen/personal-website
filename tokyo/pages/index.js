98a31d9926ac6acf65e6c28c1329b51055c01a0e
Sofus Wenøe <sofuswenoee@gmail.com>
2024-12-09 14:28:45 +0900

    Update index.js

--- tokyo/pages/index.js
+++ tokyo/pages/index.js
@@ -2,7 +2,7 @@ import Layout from "@/src/layout/Layout";
 import About from "components/About";
 import Contact from "components/Contact";
 import Home from "components/Home";
-import Blog from "components/Blog";
+{/*import Blog from "components/Blog";*/}
 import Service from "components/Service";
 import dynamic from "next/dynamic";
 const Portfolio = dynamic(() => import("components/Portfolio"), {
